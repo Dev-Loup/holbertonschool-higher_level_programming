9657946fb70aaabb1a7b7f578fc228647fe210d7
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-04-27 18:35:26 +0000

    [fix]: fix issues with exercises
    exercise 0 and 1 must be shell scripts not python scripts
    exercise 5 missing the last Holberton printing
    exercise 6 corrected betty style
    exercise 7 missing one letter in the first 3 letters
    exercise 10 Betty style corrected

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,7 +2,7 @@
 
 /**
  * check_cycle - checks if a linked list has a cycle in it
- * @head: pointer to head of the string
+ * @list: pointer to head of the string
  * Return: 0 if there is no cycles, 1 if a cycle is found
  **/
 int check_cycle(listint_t *list)
@@ -14,11 +14,11 @@ int check_cycle(listint_t *list)
 	while (rabbit)
 	{
 		turtle = turtle->next;
-		for(i = 0; i <= 1; i++)
+		for (i = 0; i <= 1; i++)
 		{
 			rabbit = rabbit->next;
 		}
-		if(rabbit == turtle)
+		if (rabbit == turtle)
 			return (1);
 	}
 	return (0);
