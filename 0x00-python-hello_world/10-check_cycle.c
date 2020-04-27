ff2cd3ff4b4564ae12594691c2ac384ca13de6a1
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-04-27 18:02:51 +0000

    [add]: Add newbie exercises in python script

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a linked list has a cycle in it
+ * @head: pointer to head of the string
+ * Return: 0 if there is no cycles, 1 if a cycle is found
+ **/
+int check_cycle(listint_t *list)
+{
+	listint_t *turtle = list;
+	listint_t *rabbit = list;
+	int i = 0;
+
+	while (rabbit)
+	{
+		turtle = turtle->next;
+		for(i = 0; i <= 1; i++)
+		{
+			rabbit = rabbit->next;
+		}
+		if(rabbit == turtle)
+			return (1);
+	}
+	return (0);
+}
