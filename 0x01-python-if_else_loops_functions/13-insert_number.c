07ca2ea9d46c60f04f33a076b9902281fde7d3c2
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-04-29 04:33:01 +0000

    [add]: add int nodes in a sorted list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+ * insert_node - adds a new int node in numeric order
+ * @head: pointer to sorted list
+ * @number: int to add
+ * Return: pointer to list
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *cpy;
+	listint_t *ptr;
+
+	ptr = malloc(sizeof(listint_t));
+	if (ptr != NULL)
+		return (NULL);
+	cpy = *head;
+	ptr->n = number;
+	if (!*head || cpy->n > number)
+	{
+		ptr->next = *head;
+		*head = ptr;
+		return (ptr);
+	}
+	while (cpy->next)
+	{
+		if (cpy->next->n >= number)
+		{
+			ptr->next = cpy->next;
+			cpy->next = ptr;
+			return (ptr);
+		}
+		tmp = tmp->next;
+	}
+	ptr->next = NULL;
+	cpy->next = ptr;
+	return (ptr);
+}
