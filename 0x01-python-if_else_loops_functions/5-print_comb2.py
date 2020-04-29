5fcc692a48b9e47c738affdbe8fd2a24fce97adf
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-04-29 04:05:14 +0000

    [fix] Put if statement comparison in parentheses

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100):
-    print("{:0>2d}".format(num), end="")
-    if (num != 99):
-        print(", ", end="")
+    if num != 99:
+        print("{:0>2d}, ".format(num), end="")
+    else:
+        print("{:0>2d}".format(num), end="")
