85b00d2138dbfd72d3db5ae5449019a18d18f9e0
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-04-30 22:03:48 +0000

    [fix] add if __name__ == "__main__"
    some functions hadn´t if statement for preventing function from being executed at import

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from sys import argv
-num = 0
-for arg in argv[1:]:
-    if arg != 0:
-        num += int(arg)
-print("{:d}".format(num))
+if __name__ == "__main__":
+    num = 0
+    for arg in argv[1:]:
+        if arg != 0:
+            num += int(arg)
+    print("{:d}".format(num))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
-name = dir(hidden_4)
-for iter in name:
-    if iter[0] != '_':
-        print(iter)
+if __name__ == "__main__":
+    name = dir(hidden_4)
+    for iter in name:
+        if iter[0] != '_':
+            print(iter)
