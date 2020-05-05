7d4cf5eabc3a587ef1621134f48463010aa33a44
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-05 19:51:19 +0000

    [fix] add missing space separator

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for X in matrix:
         for Y in X:
-            print('{:d}'.format(Y), end="")
+            print('{}'.format(Y), end="")
+            if Y != X[-1]:
+                print(' ', end="")
         print()
-    print()
