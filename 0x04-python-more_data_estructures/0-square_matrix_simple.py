42cabf342efb82ffe39444537d0d4dc05c336f30
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-07 20:15:38 +0000

    [clean] deletes junk folder

--- 0x04-python-more_data_estructures/0-square_matrix_simple.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    nmatrix = []
-    for line in matrix:
-        nmatrix.append(list(line))
-    for line in nmatrix:
-        for number in range(len(line)):
-            line[number] = line[number] ** 2
-    return nmatrix
