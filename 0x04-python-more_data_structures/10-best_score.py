df1a41a13e61e5910015bdc26a3419b2844a8c52
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-08 02:46:50 +0000

    [code]: reduce return breakpoints

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None:
-        return None
-    score = 0
-    for key in a_dictionary.keys():
-        value = a_dictionary[key]
-        if value > score:
-            score = value
-            topper = key
-    return topper
+        topper = None
+        score = 0
+        for key in a_dictionary.keys():
+            value = a_dictionary[key]
+            if value > score:
+                score = value
+                topper = key
+        return topper
