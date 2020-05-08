f5f63d8445b4c02e29caccd59ed1b7e718b1d12a
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-08 02:37:12 +0000

    [fix]: bad code, parentheses after for statement deleted

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None:
-        return None
+        return a_dictionary
     score = 0
-    for key in a_dictionary.keys()):
+    for key in a_dictionary.keys():
         value = a_dictionary[key]
         if value > score:
             score = value
