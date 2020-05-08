9f46d806ddf5b72a84339de5bc3b5b5ce287406f
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-08 00:09:03 +0000

    [update]: Switch to update method
    New concept addition to dictionary is performed by update method

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     if key:
-        a_dictionary['key'] = value
+        concept = {key: value}
+        a_dictionary.update(concept)
     return a_dictionary
