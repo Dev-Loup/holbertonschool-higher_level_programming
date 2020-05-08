15ba3beb2c64901b76480c224fc403429cbe4a2d
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-08 02:00:55 +0000

    [efficiency]: Supress sorted function being unnecesary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     score = 0
-    for key in sorted(a_dictionary.keys()):
+    for key in a_dictionary.keys()):
         value = a_dictionary[key]
         if value > score:
             score = value
