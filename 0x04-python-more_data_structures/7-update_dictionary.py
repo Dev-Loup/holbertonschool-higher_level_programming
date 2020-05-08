574e8de32357da4f4943750dbe4ac8efe6d8bcd3
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-08 00:24:51 +0000

    [efficiency]: Reduce instructions to do

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key:
-        concept = {key: value}
-        a_dictionary.update(concept)
+    concept = {key: value}
+    a_dictionary.update(concept)
     return a_dictionary
