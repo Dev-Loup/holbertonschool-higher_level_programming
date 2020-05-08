7c021dbaefcc0e3ed8b6140b7c71d3385840f2f5
Diego Romero <60360505+diego-9407@users.noreply.github.com>
2020-05-08 00:11:06 -0500

    add

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,7 @@
 def roman_to_int(roman_string):
     arab = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     values = {}
+    counter = 0
     product = 0
     for letter in roman_string:
         if values.get(letter):
