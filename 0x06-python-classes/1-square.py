881466ef372880ab91a05020bdc653801296345d
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-19 21:52:48 +0000

    [codestyle]: set documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 class Square:
+"""Class Square declaration
+makes an square type class
+"""
     def __init__(self, size):
+        """__init__ initialize the class Square with size
+        Args:
+        size (int): Size if the square
+        """
         self.size = size
