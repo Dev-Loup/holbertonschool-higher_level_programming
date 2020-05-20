c52d81012a31763b46d59b9b4bf6d35ec3fcdf05
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-20 01:49:57 +0000

    [add] square position based on white spaces and break lines

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+""" Class Square creation module
+    A Blueprint for squares
+"""
+
+
+class Square:
+    """Set the class square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Iniatiate Attributes for Square class.
+        Args:
+          size: integer with size of the square.
+          position: tuple of square location
+        """
+
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """Get the private size value.
+        Return:
+          self._size: value of size
+        """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set size into class object.
+        Args:
+          value: size to check
+        Raises:
+          ValueError: if size is lesser than 0.
+          TypeError: if size is not an integer.
+        """
+
+        if type(value) is int:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    @property
+    def position(self):
+        """Get position value.
+        Return:
+          gotten position
+        """
+
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set position value.
+        Args:
+          value: data of position
+        """
+
+        if type(value) is not tuple or value[2] is not None:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) is not int and type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 and value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+    def area(self):
+        """Square Area method.
+        Return:
+          The Area of the square.
+        """
+
+        return self.__size ** 2
+
+    def my_print(self):
+        """Printing method.
+           My_print generates an # composed square
+        """
+        if self.__size > 0:
+            if self.__position[1] is not 0:
+                print("\n" * self.__position[0], end="")
+            for X in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
+        else:
+            print()
