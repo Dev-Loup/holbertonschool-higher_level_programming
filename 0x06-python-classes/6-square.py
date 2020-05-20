997c113aee984a1b7faf107bea02f108f8e54349
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-20 01:56:01 +0000

    [add]: fixed missing __position definition after if statements

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -65,6 +65,9 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif value[0] < 0 and value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
     def area(self):
         """Square Area method.
         Return:
