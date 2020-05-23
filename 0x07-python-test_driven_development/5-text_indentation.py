934046381dc99318300921aa0b469990bdd9e9cc
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-22 22:10:59 -0500

    [add]: indent strings with test document

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+  4-text_indentation module.
+  Indent strings
+  Functions:
+    text_indentation(text)
+"""
+
+
+def text_indentation(text):
+    """
+        Prints a text with 2 new lines after
+        each of these characters: ., ? and :
+        Args:
+          text: text to indent
+        Raises:
+          TypeError: text must be an string
+    """
+    fixed = []
+    idx = 0
+    lim = ('.', '?', ':')
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    for idx in range(len(text)):
+        if text[idx] in lim:
+            fixed.append("\n")
+            fixed.append("\n")
+            continue
+        if text[idx] is " ":
+            if text[idx - 1] in lim or text[idx + 1] in lim:
+                continue
+        fixed.append(text[idx])
+    fixed = ''.join(map(str, fixed))
+    print(fixed)
