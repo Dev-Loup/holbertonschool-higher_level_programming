356e4a59ee5da0077e54ef2230fde8a36cf02dc7
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-22 19:37:18 -0500

    [add]: third tested exercise

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+  3-say_my_name module.
+  Print the name given in arguments
+  Functions:
+    say_my_name(first_name, last_name="")
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+        say_my_name prints arguments as
+        person introduction.
+        Args:
+          first_name: the main name
+          last_name: optional last name
+        Raises:
+          TypeError: first_name must be a string
+          TypeError: last_name must be a string
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is " + first_name + last_name)
