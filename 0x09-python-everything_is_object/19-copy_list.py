d48b9f94f01f254df7935368f5ac82d5aa586e23
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-26 16:46:03 -0500

    [add]: test python

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""copy_list module
+    Functions:
+        copy_list: returns a copy of list.
+"""
+
+
+def copy_list(l):
+    """copy_list func.
+    Args:
+        l: list to copy
+    """
+
+    return list(l)
