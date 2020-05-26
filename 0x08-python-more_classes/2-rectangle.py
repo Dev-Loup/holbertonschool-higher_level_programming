9ca717850540dd4c7f6e70b5bbbdedde7941edab
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-26 01:53:21 +0000

    [fix]: raising conditions changed

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -15,13 +15,6 @@ class Rectangle:
         Args:
             width: rectangle base
             Height: rectangle height
-        Raises:
-            TypeError: width must be an integer
-            ValueError: width must be >= 0
-            TypeError: height must be an integer
-            ValueError: height must be >= 0
-        self.width = width
-        self.height = height
         """
 
         self.width = width
@@ -44,22 +37,28 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ set width value on class
+        Raises:
+            TypeError: width must be an integer
+            ValueError: width must be >= 0
         """
 
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value > 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @height.setter
     def height(self, value):
         """ set height value on class
+        Raises:
+            TypeError: height must be an integer
+            ValueError: height must be >= 0
         """
 
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value > 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
