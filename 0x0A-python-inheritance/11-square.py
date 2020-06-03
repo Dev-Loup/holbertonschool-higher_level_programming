569053f696d406b4392c6f0c63bb3ae5e6f0fd81
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-06-02 20:47:31 -0500

    [add]: return square size

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module square morphing from
+    rectangle
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square Class with Rectangle base
+    """
+
+    def __init__(self, size):
+        """ Define the new Square
+            Args:
+                Size: length value
+        """
+
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """ Returns square size """
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
