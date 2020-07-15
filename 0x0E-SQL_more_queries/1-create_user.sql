49eb3b7d092870c19c9f61763f687cd55957d009
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-07-14 20:52:33 -0500

    [add]: table force set

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- user creator
+-- creates mySQL user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
