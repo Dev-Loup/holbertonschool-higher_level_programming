58dda13b14c11d08aa2fa81646adec8571a97323
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-08-09 14:14:53 -0500

    [add]: mysqldb all states query from python

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" States Module
+    Contains States MySql query functions
+"""
+
+import sys
+import MySQLdb
+
+
+def all_states():
+    """ Print all States from database
+    """
+
+    database = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        host='localhost',
+        port=3306
+    )
+    cursor = database.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    for row in states:
+        print(row)
+
+
+if __name__ == '__main__':
+    all_states()
