70baa7adce1e95083c1a6b9f39a0ce11472263ae
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-08-09 18:01:38 -0500

    [add]: filter N states with regexp

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" States Module
+    Contains States MySql query functions
+"""
+
+import sys
+import MySQLdb
+
+
+def n_states():
+    """ Print filtered by N States from database
+    """
+
+    database = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        host='localhost',
+        port=3306
+    )
+    cursor = database.cursor()
+    cursor.execute("SELECT *\
+                    FROM states\
+                    WHERE name REGEXP '^[N]'\
+                    ORDER BY id ASC")
+    states = cursor.fetchall()
+    cursor.close()
+    database.close()
+    for row in states:
+        print(row)
+
+
+if __name__ == '__main__':
+    n_states()
