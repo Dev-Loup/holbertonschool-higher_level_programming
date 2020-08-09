49e2e80a436301d68020717093fc2b507c438447
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-08-09 18:44:36 -0500

    [add]: find states by specific name

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" States Module
+    Contains States MySql query functions
+"""
+
+import sys
+import MySQLdb
+
+
+def find_states():
+    """ Filter states with a given state name
+        Sys Args:
+            1: user
+            2: password
+            3: database
+            4: state name
+    """
+
+    database = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        host='localhost',
+        port=3306
+    )
+    cursor = database.cursor()
+    cursor.execute(str(f"SELECT *\
+                       FROM states\
+                       WHERE name LIKE '{sys.argv[4]}'\
+                       ORDER BY id ASC"))
+    states = cursor.fetchall()
+    for row in states:
+        print(row)
+    cursor.close()
+    database.close()
+
+
+if __name__ == '__main__':
+    find_states()
