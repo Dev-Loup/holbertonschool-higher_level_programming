d4cc0f1c906ac8335abcabdc83a0844c9cd286d4
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-08-18 18:21:20 -0500

    [fix]: changed ip to dynamic argument

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Get the header, take the Content line & select lenght
-curl -sI 0.0.0.0:5000 | grep "Content-Length" | cut -d ' ' -f2
+curl -sI $1 | grep "Content-Length" | cut -d ' ' -f2
