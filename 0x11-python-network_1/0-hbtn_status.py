067e9bbd1f81b03fd04999e0b1a0e160a8241557
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-08-24 12:59:53 -0500

    [fix]: body res replaced with body response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,8 +6,7 @@
 import urllib.request
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
-    print(f""" Body res:
-               \t- type: {type(html)}
-               \t- content: {html}
-               \t- utf8 content: {html.decode('utf8')}
-               """)
+    print(f"""Body response:
+           \t- type: {type(html)}
+           \t- content: {html}
+           \t- utf8 content: {html.decode('utf8')}""")
