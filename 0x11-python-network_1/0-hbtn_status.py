d07d4d7b450fea01972f7db372c5764ebaed7619
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-08-24 13:19:03 -0500

    [fix]: extra breakline

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
         print("Body response:\n\
                \t- type: {}\n\
                \t- content: {}\n\
-               \t- utf8 content: {}\n"
+               \t- utf8 content: {}"
               .format(type(html), html, html.decode('utf-8')))
