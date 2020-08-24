177e5febfca0e904abb5b192cad134cec75460ff
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-08-24 12:41:56 -0500

    [add]: fetch the holby URL

---
+++ 0x11-python_network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Request module
+    Here is for making requests
+"""
+
+import urllib.request
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    print(f""" Body res:
+               \t- type: {type(html)}
+               \t- content: {html}
+               \t- utf8 content: {html.decode('utf8')}
+               """)
