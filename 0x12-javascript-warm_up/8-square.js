eb82f49b17f777408aade4f5e1c6b359650c72df
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-09-15 20:42:37 +0000

    [add]: print squares

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+let siz;
+let ySiz;
+
+if (isNaN(siz = parseInt(process.argv[2]))) {
+  console.log('Missing size');
+}
+for (ySiz = siz; ySiz > 0; ySiz--) {
+  console.log('x'.repeat(siz));
+}
