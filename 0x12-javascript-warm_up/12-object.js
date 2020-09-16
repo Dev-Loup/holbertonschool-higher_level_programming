5e910eefef5637ec2ff486530d4a30c83782bb9e
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-09-16 03:52:58 +0000

    [add]: fix issues

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,17 +1,8 @@
 #!/usr/bin/node
-let first = 0;
-let second;
-let siz = process.argv.length;
-
-if (siz < 4) {
-  second = first;
-} else {
-  for (; siz > 4; siz--) {
-    if (parseInt(process.argv[siz - 1]) > first) {
-      second = first;
-      first = parseInt(process.argv[siz - 1]);
-      console.log(second + ' y ' + first + siz);
-    }
-  }
-}
-console.log(second);
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
