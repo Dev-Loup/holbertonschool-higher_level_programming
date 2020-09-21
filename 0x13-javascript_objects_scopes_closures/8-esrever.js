ed52d9abd35fab5b1a6379b81d2d4b4f950efaf3
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-09-21 23:10:50 +0000

    [codestyle]: fix semistandard code style

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let listRev = [];
+  const listRev = [];
   for (let iter = list.length - 1; iter >= 0; iter--) {
     listRev.push(list[iter]);
   }
   return listRev;
-}
+};
