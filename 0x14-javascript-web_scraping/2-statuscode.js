b53381fb748e262e03bd9a65815936c8d74342ac
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-09-22 21:54:18 +0000

    [fix]: add body?

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
 const urlElement = process.argv[2];
-request(urlElement, function (error, response) {
+request(urlElement, function (error, response, body) {
   if (error) {
     return console.log(error);
   }
-  console.log('code: ', response && response.statusCode);
+  console.log('code: ', response.statusCode);
 });
