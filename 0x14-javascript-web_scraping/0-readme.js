02993d834b902095ea07bef1f20d86a51be84c50
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-09-22 19:09:58 +0000

    [fix]: readfile isntance from var to const

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let path = process.argv[2];
-var fs = require('fs');
+const path = process.argv[2];
+const fs = require('fs');
 
-fs.readFile(path, 'utf8', (err, data) => {
+fs.readFile(path, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
