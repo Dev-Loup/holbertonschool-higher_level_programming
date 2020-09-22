6879bd8a0e2531df0fc4441113b549179740b927
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-09-22 19:03:53 +0000

    [fix]: changed from function to arrow_function

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 let path = process.argv[2];
-if (typeof path === 'undefined') {
-  path = '';
-}
 var fs = require('fs');
-fs.readFile(path, 'utf8', function (err, data) {
+
+fs.readFile(path, 'utf8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
