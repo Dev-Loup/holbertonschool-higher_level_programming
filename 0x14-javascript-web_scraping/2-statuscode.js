713861cfad521ea8fa4dca0f1f270d267d4df004
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-09-22 21:45:28 +0000

    [fix]: return after reaching error

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@ const request = require('request');
 const urlElement = process.argv[2];
 request(urlElement, function (error, response) {
   if (error) {
-    console.error(error);
+    return console.error(error);
   }
   console.log('code: ', response && response.statusCode);
 });
