f53633b693c093a4130b057fc35d046b469b900d
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-09-22 22:13:08 +0000

    [add]: fix request function

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
 const urlElement = process.argv[2];
-request(urlElement, function (error, response, body) {
+request.get(urlElement, function (error, response) {
   if (error) {
     return console.log(error);
   }
-  console.log('code: ', response.statusCode);
+  console.log('code: %d', response && response.statusCode);
 });
